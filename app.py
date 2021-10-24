86e0295216271d8f44b67ea41a4d01b0865dd818
burzamiszcz <burzynskihubert@gmail.com>
2021-10-24 15:29:17 +0200

    navbar + start template + login from variables

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import (Flask,
-                    render_template)
+                    render_template,
+                    request,
+                    redirect)
+from flask.helpers import url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+
+admin_username, admin_password = 'admin', 'admin'
+@app.route('/', methods=['GET', 'POST'])
 def main():
+    if request.method == 'POST':
+        username = request.form['username'].lower()
+        password = request.form['password']
+        if username == admin_username and password == admin_password:
+            print(username)
+            return redirect(url_for('start', username = username))
+        else:
+            pass
     return render_template('login.html')
+
+@app.route('/<username>')
+def start(username):
+    username=username
+    return render_template('start.html')
